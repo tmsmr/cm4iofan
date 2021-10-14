7afaa7658f492316b39497a634ffdba8f9273363
Thomas Maier <contact@thomas-maier.net>
2021-10-14 21:35:04 +0200

    general refactoring, adds GetRPM to lib, MVP for fanctl

--- cm4iofan.go
+++ cm4iofan.go
@@ -3,15 +3,35 @@ package cm4iofan
 import (
 	"errors"
 	"github.com/go-daq/smbus"
+	"math"
 )
 
 const (
-	Emc2301Bus          = 10   // cm4io.pdf: 2.9 Fan connector
-	Emc2301Addr         = 0x2F // cm4io.pdf: 2.9 Fan connector
-	Emc2301RegProductId = 0xFD // emc2301.pdf: TABLE 6-1
-	Emc2301ProductId    = 0x37 // emc2301.pdf: TABLE 6-1
-	Emc2301RegConfig    = 0x32 // emc2301.pdf: 4.1 Fan Control Modes of Operation, 6.14 Fan Configuration Registers
-	Emc2301RegDutyCycle = 0x30 // emc2301.pdf: 4.1 Fan Control Modes of Operation, 6.12 Fan Drive Setting Register
+	/* CM4IO */
+
+	// addresses (cm4io.pdf: 2.9 Fan connector)
+	Emc2301Bus  = 10
+	Emc2301Addr = 0x2F
+
+	/* EMC2301 */
+
+	// general (emc2301.pdf: TABLE 6-1)
+	Emc2301ProductIdReg = 0xFD
+	Emc2301ProductIdVal = 0x37
+
+	// configuration (emc2301.pdf: 4.1 Fan Control Modes of Operation, 6.14 Fan Configuration Registers)
+	Emc2301ConfigReg      = 0x32
+	Emc2301ConfigENAGxBit = 7
+
+	// fan drive setting (emc2301.pdf: 6.12 Fan Drive Setting Register)
+	Emc2301DutyCycleReg = 0x30
+
+	// TACH reading (emc2301.pdf: 6.23 TACH Reading Registers)
+	Emc2301TachHighReg = 0x3E
+	Emc2301TachLowReg  = 0x3F
+
+	// TACH conversion (emc2301.pdf: EQUATION 4-3: SIMPLIFIED TACH CONVERSION)
+	Emc2301Tach2RPM = 3932160
 )
 
 type EMC2301 struct {
@@ -23,30 +43,58 @@ func New() (*EMC2301, error) {
 	if err != nil {
 		return nil, err
 	}
-	v, err := c.ReadReg(Emc2301Addr, Emc2301RegProductId)
+	v, err := c.ReadReg(Emc2301Addr, Emc2301ProductIdReg)
 	if err != nil {
 		return nil, err
 	}
-	if Emc2301ProductId != v {
-		return nil, errors.New("unexpected ProductId")
+	if Emc2301ProductIdVal != v {
+		return nil, errors.New("unexpected Product ID")
 	}
-	// ensure FSC (Fan Speed Control) mode is DISABLED
-	v, err = c.ReadReg(Emc2301Addr, Emc2301RegConfig)
+	// read fan configuration
+	v, err = c.ReadReg(Emc2301Addr, Emc2301ConfigReg)
 	if err != nil {
 		return nil, err
 	}
-	err = c.WriteReg(Emc2301Addr, Emc2301RegConfig, v|0<<7)
+	// ensure "Direct Setting mode" is active
+	v |= 0 << Emc2301ConfigENAGxBit
+	err = c.WriteReg(Emc2301Addr, Emc2301ConfigReg, v)
 	if err != nil {
 		return nil, err
 	}
 	return &EMC2301{conn: c}, nil
 }
 
-func (ctrl *EMC2301) SetDutyCycle(dc uint8) error {
-	if dc < 0 || dc > 100 {
-		return errors.New("expecting a value 0 <= x <= 100")
+func (ctrl *EMC2301) GetDutyCycle() (int, error) {
+	v, err := ctrl.conn.ReadReg(Emc2301Addr, Emc2301DutyCycleReg)
+	if err != nil {
+		return -1, err
 	}
-	// emc2301.pdf: EQUATION 4-1
-	v := 255 * (float32(dc) / 100)
-	return ctrl.conn.WriteReg(Emc2301Addr, Emc2301RegDutyCycle, uint8(v))
+	// emc2301.pdf: EQUATION 4-1: REGISTER VALUE TO DRIVE
+	return int(math.Round((float64(v) / 255) * 100)), nil
+}
+
+func (ctrl *EMC2301) SetDutyCycle(percent int) error {
+	if percent < 0 || percent > 100 {
+		return errors.New("expecting a value of 0 <= x <= 100")
+	}
+	// emc2301.pdf: EQUATION 4-1: REGISTER VALUE TO DRIVE
+	v := math.Round(255 * (float64(percent) / 100))
+	return ctrl.conn.WriteReg(Emc2301Addr, Emc2301DutyCycleReg, uint8(v))
+}
+
+func (ctrl *EMC2301) GetRPM() (int, error) {
+	h, err := ctrl.conn.ReadReg(Emc2301Addr, Emc2301TachHighReg)
+	if err != nil {
+		return -1, err
+	}
+	l, err := ctrl.conn.ReadReg(Emc2301Addr, Emc2301TachLowReg)
+	if err != nil {
+		return -1, err
+	}
+	// HIGH BYTE - Bit 7: 2048 ... Bit 0: 32
+	// LOW BYTE - Bit 7: 16 ... Bit 3: 1, Bit 0-2: ignored
+	tach := uint16(h) << 5
+	tach |= uint16(l) >> 3
+	// emc2301.pdf: EQUATION 4-3: SIMPLIFIED TACH CONVERSION
+	return int(Emc2301Tach2RPM / uint32(tach)) * 2, nil
 }
--- fanctl/fanctl.go
+++ fanctl/fanctl.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tmsmr/cm4iofan"
 	"os"
 	"strconv"
 )
 
-func main() {
-	dc, err := strconv.Atoi(os.Args[1])
+func usage() {
+	usage := `Set PWM duty cycle in %
+	$ fanctl set VALUE
+Get PWM duty cycle in %
+	$ fanctl get
+Get current (guessed) RPM:
+	$ fanctl rpm`
+	fmt.Println(usage)
+	os.Exit(1)
+}
+
+func set(ctrl *cm4iofan.EMC2301, dc int) {
+	err := ctrl.SetDutyCycle(dc)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func get(ctrl *cm4iofan.EMC2301) {
+	dc, err := ctrl.GetDutyCycle()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(dc)
+}
+
+func rpm(ctrl *cm4iofan.EMC2301) {
+	rpm, err := ctrl.GetRPM()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rpm)
+}
+
+func main() {
 	ctrl, err := cm4iofan.New()
 	if err != nil {
 		panic(err)
 	}
-	err = ctrl.SetDutyCycle(uint8(dc))
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		usage()
+	}
+	switch os.Args[1] {
+	case "set":
+		if len(os.Args) < 3 {
+			usage()
+		}
+		dc, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+		set(ctrl, dc)
+	case "get":
+		get(ctrl)
+	case "rpm":
+		rpm(ctrl)
+	default:
+		usage()
 	}
 }
